31815e1d095155808fa77e88138c5a121ceda907
bartstc <bartst010395@gmail.com>
2022-11-16 15:41:55 +0100

    Test husky 2

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-documentation">
+      <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </div>
