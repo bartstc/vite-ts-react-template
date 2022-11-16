228c89ed77f80c7c3ae39925b98ec1ddb097ae9c
bartstc <bartst010395@gmail.com>
2022-11-16 15:19:41 +0100

    Configure prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+import reactLogo from "./assets/react.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -28,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
