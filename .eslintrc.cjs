3de13b0fe05d69a85f01ade2eff56446d442c387
bartstc <bartst010395@gmail.com>
2022-11-28 14:54:41 +0100

    Add restrictions regarding imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,57 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["react-app"],
+  overrides: [
+    {
+      files: ["**/*.stories.*"],
+      rules: {
+        "import/no-anonymous-default-export": "off",
+      },
     },
-    extends: ["react-app"],
-    overrides: [
-        {
-            files: ["**/*.stories.*"],
-            rules: {
-                "import/no-anonymous-default-export": "off",
-            },
-        },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": [
+      "warn",
+      {
+        fixToUnknown: true,
+        ignoreRestArgs: true,
+      },
     ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-    plugins: ["react", "@typescript-eslint"],
-    rules: {
-        "@typescript-eslint/no-explicit-any": [
-            "warn",
-            {
-                fixToUnknown: true,
-                ignoreRestArgs: true,
-            },
+    "react/jsx-key": [
+      "warn",
+      {
+        checkFragmentShorthand: true,
+      },
+    ],
+    "no-console": [
+      "warn",
+      {
+        allow: ["warn", "error", "info"],
+      },
+    ],
+    "no-restricted-imports": [
+      "warn",
+      {
+        patterns: [
+          "shared/*/*",
+          "*/**/shared/*",
+          "utils/*/*",
+          "*/**/utils/*",
+          "modules/*/*/*",
+          "*/**/modules/*/*",
+          "dayjs",
+          "react-router-dom",
         ],
-        "react/jsx-key": [
-            "warn",
-            {
-                checkFragmentShorthand: true,
-            },
-        ],
-        "no-console": [
-            "warn",
-            {
-                allow: ["warn", "error", "info"],
-            },
-        ],
-    },
+      },
+    ],
+  },
 };
