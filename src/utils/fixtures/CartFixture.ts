c691bf6238bfaca25e3ac8a65263be5ff65a3868
bartstc <bartst010395@gmail.com>
2023-03-02 16:21:00 +0000

    format - useDateVO in CartFixture

--- src/utils/fixtures/CartFixture.ts
+++ src/utils/fixtures/CartFixture.ts
@@ -1,10 +1,12 @@
+import { DateVO } from "utils/format";
+
 import { ICart } from "modules/carts/types";
 
 import { createFixture } from "./createFixture";
 
 export const CartFixture = createFixture<ICart>({
   id: 1,
-  date: "2020-10-10",
+  date: DateVO.generateDate(),
   userId: 1,
   products: [{ productId: 1, quantity: 2 }],
 });
