822f2fe1dd7fff2ef6e375b1528aa46b6e869de6
bartstc <bartst010395@gmail.com>
2023-03-04 12:53:18 +0000

    Shared/Modal - prepare modal state factory

---
+++ src/shared/Modal/createModalStore.ts
@@ -0,0 +1,24 @@
+import create from "zustand";
+
+type IModalStore<Item> = {
+  selectedItem: Item | null;
+  isOpen: boolean;
+  onOpen(item?: Item): void;
+  onClose(): void;
+};
+
+export function createModalStore<Item>(store?: Partial<IModalStore<Item>>) {
+  return create<IModalStore<Item>>((set) => {
+    return {
+      selectedItem: null,
+      isOpen: false,
+      onOpen(item) {
+        set({ selectedItem: item ?? null, isOpen: true });
+      },
+      onClose() {
+        set({ isOpen: false, selectedItem: null });
+      },
+      ...store,
+    };
+  });
+}
---
+++ src/shared/Modal/index.ts
@@ -0,0 +1,1 @@
+export { createModalStore } from "./createModalStore";
