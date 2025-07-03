ca7dfe196cd5ffa6fccc7066c637b715943c28d2
bartstc <bartst010395@gmail.com>
2025-07-03 14:32:30 +0000

    chore: update core packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,27 +3,25 @@ import * as tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "@storybook/addon-a11y",
-    "storybook-dark-mode",
-    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
+
   viteFinal(config) {
     return {
       ...config,
       plugins: [...(config.plugins ?? []), tsconfigPaths.default()],
     };
   },
-  docs: {
-    autodocs: "tag",
-  },
+
   typescript: {
     reactDocgen: "react-docgen-typescript",
   },
