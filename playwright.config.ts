c675a782abaa818ea1de3cd675eb4f27a16976a9
bartstc <bartst010395@gmail.com>
2026-01-13 19:10:03 +0000

    chore: configure relative imports for e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
+    baseURL: process.env.BASE_URL || "http://localhost:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
