f45b02df863b2cd87ac8890b9d0f7a7d25a0e3e2
bartstc <bartst010395@gmail.com>
2026-04-14 14:18:04 +0000

    chore: fix loading products error

--- src/features/products/providers/products-query.ts
+++ src/features/products/providers/products-query.ts
@@ -1,3 +1,5 @@
+import { keepPreviousData } from "@tanstack/react-query";
+
 import { productsQuery } from "@/lib/api/products/products-list/products-list-query";
 import { useQuery } from "@/lib/query";
 import type { QueryParams } from "@/types/query-params";
@@ -6,4 +8,4 @@ export { productsLoader } from "@/lib/api/products/products-list/products-list-q
 export { productsQuery };
 
 export const useProductsQuery = (params?: QueryParams) =>
-  useQuery(productsQuery(params));
+  useQuery({ ...productsQuery(params), placeholderData: keepPreviousData });
