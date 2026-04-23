e7627a68c04c1cce69397f28c328594d3867147b
bartstc <bartst010395@gmail.com>
2026-04-23 21:04:24 +0000

    chore: separate vitest setup fro unit tests

--- test-setup.ts
+++ test-setup.ts
@@ -1,20 +1,13 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
-import { afterAll, afterEach, beforeAll } from "vitest";
+import { afterEach, beforeAll } from "vitest";
 
 import { initializeI18n } from "@/test-lib/init-i18n";
-import { mswServer } from "@/test-lib/msw-server";
 
 beforeAll(async () => {
   await initializeI18n();
-  mswServer.listen({ onUnhandledRequest: "error" });
 });
 
 afterEach(() => {
   cleanup();
-  mswServer.resetHandlers();
-});
-
-afterAll(() => {
-  mswServer.close();
 });
